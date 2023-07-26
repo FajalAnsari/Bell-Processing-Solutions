ebad0511b9a120b9a1aecc86534aa3774a52620f
Garvit <garvitbansal2303@gmail.com>
2023-07-26 10:43:52 +0530

    Service Cards added

--- JAVASCRIPT/main.js
+++ JAVASCRIPT/main.js
@@ -5,3 +5,17 @@ document.addEventListener('DOMContentLoaded', function() {
       wrap: true // Set to false if you want the carousel to stop at the last slide
     });
   });
+
+//Typing animation for the sliders text
+var i = 0;
+var txt = 'A Leader in the Credit Card Processing Industry ';
+var speed = 50;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementsByClassName("typed").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+//Typing animation for the sliders text
