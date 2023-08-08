3b2602cd0f080d0df669a6e91ef21d43c44bebc2
Junaid Khan <me.zunaidkhan@gmail.com>
2023-08-08 13:56:32 +0530

    Form Validation Updated

--- JAVASCRIPT/main.js
+++ JAVASCRIPT/main.js
@@ -74,10 +74,10 @@ const selectError3 = document.getElementById('selectError3');
 
 
 form.addEventListener('submit', (e) => {
-  if(checkInputs()){
+  if (checkInputs()) {
     alert("Form Submitted")
   }
-  else{
+  else {
     e.preventDefault();
   }
 });
@@ -85,16 +85,17 @@ form.addEventListener('submit', (e) => {
 
 
 function checkInputs() {
-  var p1 = [checkFullNameValidity(),checkFullName1Validity(),  checkEmailValidity(),  checkPhoneValidity(),checkPhone1Validity(),checkSelectValidity(),checkSelect1Validity(),checkSelect2Validity(),checkSelect3Validity()]
-  for(i in p1){
-    if(p1[i]==false){
+  var p1 = [checkFullNameValidity(), checkFullName1Validity(), checkEmailValidity(), checkPhoneValidity(), checkPhone1Validity(), checkSelectValidity(), checkSelect1Validity(), checkSelect2Validity(), checkSelect3Validity()]
+  for (i in p1) {
+    if (p1[i] == false) {
       return false
     }
   }
   return true;
+}
 //Checking Full Name validation
 
-fullName.addEventListener('blur', checkFullNameValidity);
+fullName.addEventListener('change', checkFullNameValidity);
 function checkFullNameValidity() {
   const fullNameValue = fullName.value.trim();
   var regex = /^[a-zA-Z ]{3,30}$/;
@@ -103,15 +104,11 @@ function checkFullNameValidity() {
     setValidClass(fullName);
     return true
   }
-  else {
-    setError(fullNameError, 'Full name is required');
-    setInvalidClass(fullName);
-    return false
-  }
 }
+
 // Checking Company Name validation
- fullName1.addEventListener('blur', checkFullName1Validity);
- function checkFullName1Validity() {
+fullName1.addEventListener('change', checkFullName1Validity);
+function checkFullName1Validity() {
   const fullNameValue = fullName1.value.trim();
   var regex = /^[a-zA-Z ]{3,30}$/;
   if (regex.test(fullNameValue)) {
@@ -128,161 +125,160 @@ function checkFullNameValidity() {
 }
 
 
-  
 
 
-  // Checking Email Validation
-  email.addEventListener('blur', checkEmailValidity);
-  function checkEmailValidity() {
-    const emailValue = email.value.trim();
 
-    if (emailValue === '') {
-      setError(emailError, 'Email is required');
-      setInvalidClass(email);
-      return false
-    } else if (!isValidEmail(emailValue)) {
-      setError(emailError, 'Please enter a valid email address');
-      setInvalidClass(email);
-      return false
-    } else {
-      clearError(emailError);
-      setValidClass(email);
-      return true
-    }
+// Checking Email Validation
+email.addEventListener('change', checkEmailValidity);
+function checkEmailValidity() {
+  const emailValue = email.value.trim();
+
+  if (emailValue === '') {
+    setError(emailError, 'Email is required');
+    setInvalidClass(email);
+    return false
+  } else if (!isValidEmail(emailValue)) {
+    setError(emailError, 'Please enter a valid email address');
+    setInvalidClass(email);
+    return false
+  } else {
+    clearError(emailError);
+    setValidClass(email);
+    return true
   }
+}
 
-  function isValidEmail(email) {
-    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+function isValidEmail(email) {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
+
+
+
+//Checking Mobile Number Validation of both Primary and Secondary Number
+
+//Primary Phone Number
+phone.addEventListener('change', checkPhoneValidity);
+function checkPhoneValidity() {
+  const phoneValue = phone.value.trim();
+
+  if (phoneValue === '') {
+    setError(phoneError, 'Primary Phone Number is required');
+    setInvalidClass(phone);
+    return false
+  } else if (!isValidPhone(phoneValue)) {
+    setError(phoneError, 'Please enter a valid 10-digit Mobile number');
+    setInvalidClass(phone);
+    return false
+  } else {
+    clearError(phoneError);
+    setValidClass(phone);
+    return true
   }
+}
+//Secondary Phone Number
+phone1.addEventListener('change', checkPhone1Validity);
+function checkPhone1Validity() {
+  const phoneValue = phone1.value.trim();
 
-
-
-  //Checking Mobile Number Validation of both Primary and Secondary Number
-
-  //Primary Phone Number
-  phone.addEventListener('blur', checkPhoneValidity);
-  function checkPhoneValidity() {
-    const phoneValue = phone.value.trim();
-
-    if (phoneValue === '') {
-      setError(phoneError, 'Primary Phone Number is required');
-      setInvalidClass(phone);
-      return false
-    } else if (!isValidPhone(phoneValue)) {
-      setError(phoneError, 'Please enter a valid 10-digit Mobile number');
-      setInvalidClass(phone);
-      return false
-    } else {
-      clearError(phoneError);
-      setValidClass(phone);
-      return true
-    }
+  if (phoneValue === '') {
+    setError(phoneError1, 'Secondary Phone Number is required');
+    setInvalidClass(phone1);
+    return false
+  } else if (!isValidPhone(phoneValue)) {
+    setError(phoneError1, 'Please enter a valid 10-digit Mobile number');
+    setInvalidClass(phone1);
+    return false
+  } else {
+    clearError(phoneError1);
+    setValidClass(phone1);
+    return true
   }
-  //Secondary Phone Number
-  phone1.addEventListener('blur', checkPhone1Validity);
-  function checkPhone1Validity() {
-    const phoneValue = phone1.value.trim();
+}
 
-    if (phoneValue === '') {
-      setError(phoneError1, 'Secondary Phone Number is required');
-      setInvalidClass(phone1);
-      return false
-    } else if (!isValidPhone(phoneValue)) {
-      setError(phoneError1, 'Please enter a valid 10-digit Mobile number');
-      setInvalidClass(phone1);
-      return false
-    } else {
-      clearError(phoneError1);
-      setValidClass(phone1);
-      return true
-    }
+function isValidPhone(phone1) {
+  return /^\d{10}$/.test(phone1);
+}
+
+
+//Checking Select Option Validation for STATE
+selectOption.addEventListener('change', checkSelectValidity);
+function checkSelectValidity() {
+  const selectedOption = selectOption.value;
+
+  if (selectedOption === '') {
+    setError(selectError, 'Please select an option');
+    setInvalidClass(selectOption);
+    return false
+  } else {
+    clearError(selectError);
+    setValidClass(selectOption);
+    return true
   }
+}
+//Checking Select Option Validation Credit cards Yes / No
+selectOption1.addEventListener('change', checkSelect1Validity);
+function checkSelect1Validity() {
+  const selectedOption = selectOption1.value;
 
-  function isValidPhone(phone1) {
-    return /^\d{10}$/.test(phone1);
+  if (selectedOption === '') {
+    setError(selectError1, 'Please select an option');
+    setInvalidClass(selectOption1);
+    return false
+  } else {
+    clearError(selectError1);
+    setValidClass(selectOption1);
+    return true
   }
+}
+//Checking Select Option Validation Of Credit Card Sales option
+selectOption2.addEventListener('change', checkSelect2Validity);
+function checkSelect2Validity() {
+  const selectedOption = selectOption2.value;
 
-
-  //Checking Select Option Validation for STATE
-  selectOption.addEventListener('change', checkSelectValidity);
-  function checkSelectValidity() {
-    const selectedOption = selectOption.value;
-
-    if (selectedOption === '') {
-      setError(selectError, 'Please select an option');
-      setInvalidClass(selectOption);
-      return false
-    } else {
-      clearError(selectError);
-      setValidClass(selectOption);
-      return true
-    }
+  if (selectedOption === '') {
+    setError(selectError2, 'Please select an option');
+    setInvalidClass(selectOption2);
+    return false
+  } else {
+    clearError(selectError2);
+    setValidClass(selectOption2);
+    return true
   }
-  //Checking Select Option Validation Credit cards Yes / No
-  selectOption1.addEventListener('change', checkSelect1Validity);
-  function checkSelect1Validity() {
-    const selectedOption = selectOption1.value;
+}
+//Checking Select Option Validation Of Time in Business Option
+selectOption3.addEventListener('change', checkSelect3Validity);
+function checkSelect3Validity() {
+  const selectedOption = selectOption3.value;
 
-    if (selectedOption === '') {
-      setError(selectError1, 'Please select an option');
-      setInvalidClass(selectOption1);
-      return false
-    } else {
-      clearError(selectError1);
-      setValidClass(selectOption1);
-      return true
-    }
+  if (selectedOption === '') {
+    setError(selectError3, 'Please select an option');
+    setInvalidClass(selectOption3);
+    return false
+  } else {
+    clearError(selectError3);
+    setValidClass(selectOption3);
+    return true
   }
-  //Checking Select Option Validation Of Credit Card Sales option
-  selectOption2.addEventListener('change', checkSelect2Validity);
-  function checkSelect2Validity() {
-    const selectedOption = selectOption2.value;
+}
 
-    if (selectedOption === '') {
-      setError(selectError2, 'Please select an option');
-      setInvalidClass(selectOption2);
-      return false
-    } else {
-      clearError(selectError2);
-      setValidClass(selectOption2);
-      return true
-    }
-  }
-  //Checking Select Option Validation Of Time in Business Option
-  selectOption3.addEventListener('change', checkSelect3Validity);
-  function checkSelect3Validity() {
-    const selectedOption = selectOption3.value;
+// Error And Success Functions to set Invalid and Valid classes  
+function setError(element, message) {
+  element.textContent = message;
+  element.classList.add('text-danger')
+}
 
-    if (selectedOption === '') {
-      setError(selectError3, 'Please select an option');
-      setInvalidClass(selectOption3);
-      return false
-    } else {
-      clearError(selectError3);
-      setValidClass(selectOption3);
-      return true
-    }
-  }
+function clearError(element) {
+  element.textContent = '';
+}
 
-  // Error And Success Functions to set Invalid and Valid classes  
-  function setError(element, message) {
-    element.textContent = message;
-    element.classList.add('text-danger')
-  }
+function setInvalidClass(input) {
+  input.classList.add('is-invalid');
+  input.classList.remove('is-valid');
+}
 
-  function clearError(element) {
-    element.textContent = '';
-  }
-
-  function setInvalidClass(input) {
-    input.classList.add('is-invalid');
-    input.classList.remove('is-valid');
-  }
-
-  function setValidClass(input) {
-    input.classList.remove('is-invalid');
-    input.classList.add('is-valid');
-  }
+function setValidClass(input) {
+  input.classList.remove('is-invalid');
+  input.classList.add('is-valid');
 }
 
 
