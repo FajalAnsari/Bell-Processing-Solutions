5d6f3fa38e3671d3d7564adcb8383cde3c02520e
Junaid Khan <me.zunaidkhan@gmail.com>
2023-08-08 12:50:38 +0530

    Validation Completed

--- JAVASCRIPT/main.js
+++ JAVASCRIPT/main.js
@@ -17,13 +17,13 @@ $(document).ready(function () {
 
 ///////////////////////////////////////////////CAROUSEL SLIDING LOGIC//////////////////////////////////////////////////////
 
-document.addEventListener('DOMContentLoaded', function() {
-    var myCarousel = new bootstrap.Carousel(document.getElementById('carousel'), 
+document.addEventListener('DOMContentLoaded', function () {
+  var myCarousel = new bootstrap.Carousel(document.getElementById('carousel'),
     {
-      interval: 3000, 
+      interval: 3000,
       wrap: true // Set to false if you want the carousel to stop at the last slide
     });
-  });
+});
 
 
 
@@ -34,21 +34,21 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
-/* Faqs Section functionality started */ 
+/* Faqs Section functionality started */
 function toggleAnswer(questionId) {
   var answer = document.getElementById('answer' + questionId);
   var arrow = document.querySelector('.question:nth-child(' + questionId + ') .arrow');
-  
+
   if (answer.style.display === 'none') {
-      answer.style.display = 'block';
-      arrow.style.transform = 'rotate(-135deg)';
+    answer.style.display = 'block';
+    arrow.style.transform = 'rotate(-135deg)';
   } else {
-      answer.style.display = 'none';
-      arrow.style.transform = 'rotate(-45deg)';
+    answer.style.display = 'none';
+    arrow.style.transform = 'rotate(-45deg)';
   }
 }
 
-/* Faqs Section functionality Ended */ 
+/* Faqs Section functionality Ended */
 
 
 //////////////////////////////APPLY NOW FORM AND CONTACT US FORM VALIDATIONS LOGIC //////////////////////////////////////////////////////
@@ -74,198 +74,215 @@ const selectError3 = document.getElementById('selectError3');
 
 
 form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  checkInputs();
+  if(checkInputs()){
+    alert("Form Submitted")
+  }
+  else{
+    e.preventDefault();
+  }
 });
 
+
+
 function checkInputs() {
-    checkFullNameValidity()
-    checkFullName1Validity()
-    checkEmailValidity()
-    checkPhoneValidity()
-    checkPhone1Validity()
-    checkSelectValidity()
-    checkSelect1Validity()
-    checkSelect2Validity()
-    checkSelect3Validity()
-
-
-
-    //Checking Full Name validation
-    fullName.addEventListener('blur', checkFullNameValidity);
-    function checkFullNameValidity() {
-      const fullNameValue = fullName.value.trim();
-    
-      if (fullNameValue === '') {
-        setError(fullNameError, 'Full name is required');
-        setInvalidClass(fullName);
-      } else if (/\d/.test(fullNameValue)) {
-        setError(fullNameError, 'Full name should not contain numbers');
-        setInvalidClass(fullName);
-      } else {
-        clearError(fullNameError);
-        setValidClass(fullName);
-      }
+  var p1 = [checkFullNameValidity(),checkFullName1Validity(),  checkEmailValidity(),  checkPhoneValidity(),checkPhone1Validity(),checkSelectValidity(),checkSelect1Validity(),checkSelect2Validity(),checkSelect3Validity()]
+  for(i in p1){
+    if(p1[i]==false){
+      return false
     }
-    //Checking Company Name validation
-    fullName1.addEventListener('blur', checkFullName1Validity);
-    function checkFullName1Validity() {
-      const fullNameValue = fullName1.value.trim();
-    
-      if (fullNameValue === '') {
-        setError(fullNameError1, 'Company name is required');
-        setInvalidClass(fullName1);
-      } else if (/\d/.test(fullNameValue)) {
-        setError(fullNameError1, 'Full name should not contain numbers');
-        setInvalidClass(fullName1);
-      } else {
-        clearError(fullNameError1);
-        setValidClass(fullName1);
-      }
-    }
-    
+  }
+  return true;
+//Checking Full Name validation
 
-    //Checking Email Validation
-    email.addEventListener('blur', checkEmailValidity);
-    function checkEmailValidity() {
-      const emailValue = email.value.trim();
-    
-      if (emailValue === '') {
-        setError(emailError, 'Email is required');
-        setInvalidClass(email);
-      } else if (!isValidEmail(emailValue)) {
-        setError(emailError, 'Please enter a valid email address');
-        setInvalidClass(email);
-      } else {
-        clearError(emailError);
-        setValidClass(email);
-      }
-    }
+fullName.addEventListener('blur', checkFullNameValidity);
+function checkFullNameValidity() {
+  const fullNameValue = fullName.value.trim();
+  var regex = /^[a-zA-Z ]{3,30}$/;
+  if (regex.test(fullNameValue)) {
+    clearError(fullNameError);
+    setValidClass(fullName);
+    return true
+  }
+  else {
+    setError(fullNameError, 'Full name is required');
+    setInvalidClass(fullName);
+    return false
+  }
+}
+// Checking Company Name validation
+ fullName1.addEventListener('blur', checkFullName1Validity);
+ function checkFullName1Validity() {
+  const fullNameValue = fullName1.value.trim();
+  var regex = /^[a-zA-Z ]{3,30}$/;
+  if (regex.test(fullNameValue)) {
+    clearError(fullNameError1);
+    setValidClass(fullName1);
+    return true
+  }
+  else {
+    setError(fullNameError1, 'Company name is required');
+    setInvalidClass(fullName1);
+    return false
+  }
 
-    function isValidEmail(email) {
-      return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-    }
+}
 
 
-
-   //Checking Mobile Number Validation of both Primary and Secondary Number
-
-   //Primary Phone Number
-    phone.addEventListener('blur', checkPhoneValidity);
-    function checkPhoneValidity() {
-      const phoneValue = phone.value.trim();
-    
-      if (phoneValue === '') {
-        setError(phoneError, 'Primary Phone Number is required');
-        setInvalidClass(phone);
-      } else if (!isValidPhone(phoneValue)) {
-        setError(phoneError, 'Please enter a valid 10-digit Mobile number');
-        setInvalidClass(phone);
-      } else {
-        clearError(phoneError);
-        setValidClass(phone);
-      }
-    }
-
-    function isValidPhone(phone) {
-      return /^\d{10}$/.test(phone);
-    }
+  
 
 
+  // Checking Email Validation
+  email.addEventListener('blur', checkEmailValidity);
+  function checkEmailValidity() {
+    const emailValue = email.value.trim();
 
-    //Secondary Phone Number
-    phone1.addEventListener('blur', checkPhone1Validity);
-    function checkPhone1Validity() {
-      const phoneValue = phone1.value.trim();
-    
-      if (phoneValue === '') {
-        setError(phoneError1, 'Secondary Phone Number is required');
-        setInvalidClass(phone1);
-      } else if (!isValidPhone(phoneValue)) {
-        setError(phoneError1, 'Please enter a valid 10-digit Mobile number');
-        setInvalidClass(phone1);
-      } else {
-        clearError(phoneError1);
-        setValidClass(phone1);
-      }
+    if (emailValue === '') {
+      setError(emailError, 'Email is required');
+      setInvalidClass(email);
+      return false
+    } else if (!isValidEmail(emailValue)) {
+      setError(emailError, 'Please enter a valid email address');
+      setInvalidClass(email);
+      return false
+    } else {
+      clearError(emailError);
+      setValidClass(email);
+      return true
     }
+  }
 
-    function isValidPhone(phone1) {
-      return /^\d{10}$/.test(phone1);
-    }
+  function isValidEmail(email) {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+  }
 
 
-    //Checking Select Option Validation for STATE
-    selectOption.addEventListener('change', checkSelectValidity);
-    function checkSelectValidity() {
-      const selectedOption = selectOption.value;
-    
-      if (selectedOption === '') {
-        setError(selectError, 'Please select an option');
-        setInvalidClass(selectOption);
-      } else {
-        clearError(selectError);
-        setValidClass(selectOption);
-      }
+
+  //Checking Mobile Number Validation of both Primary and Secondary Number
+
+  //Primary Phone Number
+  phone.addEventListener('blur', checkPhoneValidity);
+  function checkPhoneValidity() {
+    const phoneValue = phone.value.trim();
+
+    if (phoneValue === '') {
+      setError(phoneError, 'Primary Phone Number is required');
+      setInvalidClass(phone);
+      return false
+    } else if (!isValidPhone(phoneValue)) {
+      setError(phoneError, 'Please enter a valid 10-digit Mobile number');
+      setInvalidClass(phone);
+      return false
+    } else {
+      clearError(phoneError);
+      setValidClass(phone);
+      return true
     }
-    //Checking Select Option Validation Credit cards Yes / No
-    selectOption1.addEventListener('change', checkSelect1Validity);
-    function checkSelect1Validity() {
-      const selectedOption = selectOption1.value;
-    
-      if (selectedOption === '') {
-        setError(selectError1, 'Please select an option');
-        setInvalidClass(selectOption1);
-      } else {
-        clearError(selectError1);
-        setValidClass(selectOption1);
-      }
+  }
+  //Secondary Phone Number
+  phone1.addEventListener('blur', checkPhone1Validity);
+  function checkPhone1Validity() {
+    const phoneValue = phone1.value.trim();
+
+    if (phoneValue === '') {
+      setError(phoneError1, 'Secondary Phone Number is required');
+      setInvalidClass(phone1);
+      return false
+    } else if (!isValidPhone(phoneValue)) {
+      setError(phoneError1, 'Please enter a valid 10-digit Mobile number');
+      setInvalidClass(phone1);
+      return false
+    } else {
+      clearError(phoneError1);
+      setValidClass(phone1);
+      return true
     }
-    //Checking Select Option Validation Of Credit Card Sales option
-    selectOption2.addEventListener('change', checkSelect2Validity);
-    function checkSelect2Validity() {
-      const selectedOption = selectOption2.value;
-    
-      if (selectedOption === '') {
-        setError(selectError2, 'Please select an option');
-        setInvalidClass(selectOption2);
-      } else {
-        clearError(selectError2);
-        setValidClass(selectOption2);
-      }
+  }
+
+  function isValidPhone(phone1) {
+    return /^\d{10}$/.test(phone1);
+  }
+
+
+  //Checking Select Option Validation for STATE
+  selectOption.addEventListener('change', checkSelectValidity);
+  function checkSelectValidity() {
+    const selectedOption = selectOption.value;
+
+    if (selectedOption === '') {
+      setError(selectError, 'Please select an option');
+      setInvalidClass(selectOption);
+      return false
+    } else {
+      clearError(selectError);
+      setValidClass(selectOption);
+      return true
     }
-    //Checking Select Option Validation Of Time in Business Option
-    selectOption3.addEventListener('change', checkSelect3Validity);
-    function checkSelect3Validity() {
-      const selectedOption = selectOption3.value;
-    
-      if (selectedOption === '') {
-        setError(selectError3, 'Please select an option');
-        setInvalidClass(selectOption3);
-      } else {
-        clearError(selectError3);
-        setValidClass(selectOption3);
-      }
+  }
+  //Checking Select Option Validation Credit cards Yes / No
+  selectOption1.addEventListener('change', checkSelect1Validity);
+  function checkSelect1Validity() {
+    const selectedOption = selectOption1.value;
+
+    if (selectedOption === '') {
+      setError(selectError1, 'Please select an option');
+      setInvalidClass(selectOption1);
+      return false
+    } else {
+      clearError(selectError1);
+      setValidClass(selectOption1);
+      return true
     }
-    
-    //Error And Success Functions to set Invalid and Valid classes  
-    function setError(element, message) {
-      element.textContent = message;
+  }
+  //Checking Select Option Validation Of Credit Card Sales option
+  selectOption2.addEventListener('change', checkSelect2Validity);
+  function checkSelect2Validity() {
+    const selectedOption = selectOption2.value;
+
+    if (selectedOption === '') {
+      setError(selectError2, 'Please select an option');
+      setInvalidClass(selectOption2);
+      return false
+    } else {
+      clearError(selectError2);
+      setValidClass(selectOption2);
+      return true
     }
-    
-    function clearError(element) {
-      element.textContent = '';
+  }
+  //Checking Select Option Validation Of Time in Business Option
+  selectOption3.addEventListener('change', checkSelect3Validity);
+  function checkSelect3Validity() {
+    const selectedOption = selectOption3.value;
+
+    if (selectedOption === '') {
+      setError(selectError3, 'Please select an option');
+      setInvalidClass(selectOption3);
+      return false
+    } else {
+      clearError(selectError3);
+      setValidClass(selectOption3);
+      return true
     }
-    
-    function setInvalidClass(input) {
-      input.classList.add('is-invalid');
-      input.classList.remove('is-valid');
-    }
-    
-    function setValidClass(input) {
-      input.classList.remove('is-invalid');
-      input.classList.add('is-valid');
-    }    
+  }
+
+  // Error And Success Functions to set Invalid and Valid classes  
+  function setError(element, message) {
+    element.textContent = message;
+    element.classList.add('text-danger')
+  }
+
+  function clearError(element) {
+    element.textContent = '';
+  }
+
+  function setInvalidClass(input) {
+    input.classList.add('is-invalid');
+    input.classList.remove('is-valid');
+  }
+
+  function setValidClass(input) {
+    input.classList.remove('is-invalid');
+    input.classList.add('is-valid');
+  }
 }
 
 
@@ -337,5 +354,3 @@ function validateStep2() {
 
   return true;
 }
-
-
